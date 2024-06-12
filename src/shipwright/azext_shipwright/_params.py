ad7a490c0f69d48edfb93e004d9a482aa3d14115
minghe <rmhsawyer>
2024-06-12 00:31:05 -0700

    add initial create vm function

--- src/shipwright/azext_shipwright/_params.py
+++ src/shipwright/azext_shipwright/_params.py
@@ -10,10 +10,23 @@ echo_back_arg_type = CLIArgumentType(
     options_list=('--echo-back', '-e'), help='Echo the input string back.')
 
 vm_name_arg_type = CLIArgumentType(
-    options_list=('--vm-name', '-n'), help='Name of the VM to create.')
+    options_list=('--vm_name', '-n'), help='Name of the VM to create.')
+
+resource_group_arg_type = CLIArgumentType(
+    options_list=('--resource_group_name', '-rg'), help='Name of the resource group that will contain the VM.')
+
+image = CLIArgumentType(
+    options_list=('--image'), help='Option to create vm from a specific image.')
+
+admin_username = CLIArgumentType(
+    options_list=('--admin_username'), help='Name of admin user for created image.')
+
 
 def load_arguments(self, _):
 
     with self.argument_context('shipwright') as c:
         c.argument('echo_back', arg_type=echo_back_arg_type)
         c.argument('vm_name', arg_type=vm_name_arg_type)
+        c.argument('resource_group_name', arg_type=resource_group_arg_type)
+        c.argument('image', arg_type=image)
+        c.argument('admin_username', arg_type=admin_username)
--- src/shipwright/azext_shipwright/custom.py
+++ src/shipwright/azext_shipwright/custom.py
@@ -3,8 +3,30 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import subprocess
+import shlex
+
+from knack.log import get_logger
+
+logger = get_logger(__name__)
+
 def echo_shipwright(cmd, echo_back):
     print(f"echo: '{echo_back}'")
 
-def create_vm_shipwright(cmd, vm_name):
-    print(f"Creating VM '{vm_name}'...")
+def create_vm_shipwright(cmd, vm_name, resource_group_name, image, admin_username):
+
+    logger.debug('Create-vm command parameters:  vm_name: %s, resource_group_name: %s, image: %s', vm_name, resource_group_name, image)
+    print(f"Creating VM '{vm_name}'... {resource_group_name}")
+
+    create_vm_command = 'az vm create -n {n} -g {g}  --tag AzSecPackAutoConfigReady=true --image {image_name} --admin-username {username}' \
+                .format(n=vm_name, g=resource_group_name, image_name=image, username=admin_username)
+
+    tokenized_command = shlex.split(create_vm_command)
+    
+    process = subprocess.Popen(tokenized_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+    stdout, stderr = process.communicate()
+
+    print(f"stdout: '{stdout}'... stderr: '{stderr}' ")
+
+
