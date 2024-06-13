f4ebc89d66adec68a3435bb00164e1ecd8b92e25
minghe <rmhsawyer>
2024-06-13 15:37:49 -0700

    add vmss warpper

--- src/shipwright/azext_shipwright/commands.py
+++ src/shipwright/azext_shipwright/commands.py
@@ -37,3 +37,14 @@ def load_command_table(self, _):
             validator=azl_process_vm_create_namespace,
             exception_handler=handle_template_based_exception,
         )
+
+    with self.command_group("shipwright vmss", compute_vm_sdk) as g:
+        g.custom_command(
+            "create",
+            "create_vmss",
+            transform=transform_vm_create_output,
+            supports_no_wait=True,
+            table_transformer=deployment_validate_table_format,
+            validator=azl_process_vm_create_namespace,
+            exception_handler=handle_template_based_exception,
+        )
