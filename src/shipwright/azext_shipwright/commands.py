ab8a203e6eecf766b0e6b5ff42b7f74400270da2
corvus-callidus <108946721+corvus-callidus@users.noreply.github.com>
2024-06-13 20:14:03 +0000

    AKS command api error

--- src/shipwright/azext_shipwright/commands.py
+++ src/shipwright/azext_shipwright/commands.py
@@ -11,15 +11,54 @@ from azure.cli.core.commands import CliCommandType
 
 from azext_shipwright.validators import azl_process_vm_create_namespace
 
+import sys
+from azure.cli.core.extension import get_extension_path
+sys.path.append(get_extension_path('aks-preview'))
+
+
 def load_command_table(self, _):
+
+    # import sys
+    # from azure.cli.core.extension import get_extension_path
+
+    # print(f"sys.path: {sys.path}")
+    # sys.path.append(get_extension_path('aks-preview'))
+
+    # print(f"sys.path: {sys.path}")
+
+
+    from azext_aks_preview._client_factory import cf_managed_clusters
+    from azext_aks_preview.commands import transform_mc_objects_with_custom_cas
+
+    print("lyrydber: load_command_table")
+
+    managed_clusters_sdk = CliCommandType(
+        operations_tmpl="azext_aks_preview.vendored_sdks.azure_mgmt_preview_aks."
+        "operations._managed_clusters_operations#ManagedClustersOperations.{}",
+        operation_group="managed_clusters",
+        client_factory=cf_managed_clusters,
+    )
+    print("lyrydber: managed_clusters_sdk")
 
     compute_vm_sdk = CliCommandType(
         operations_tmpl='azure.mgmt.compute.operations#VirtualMachinesOperations.{}',
         client_factory=cf_vm
     )
+    print("lyrydber: compute_vm_sdk")
 
     with self.command_group('shipwright') as g:
         g.custom_command('echo', 'echo_shipwright')
+    print("lyrydber: shipwright echo")
+
+    with self.command_group('shipwright aks',
+        managed_clusters_sdk,
+        client_factory=cf_managed_clusters,
+        transform=transform_mc_objects_with_custom_cas,
+    ) as g:
+        g.custom_command('create', 'aks_create', supports_no_wait=False)
+        g.custom_command('mycreate', 'shipwright_aks_create', supports_no_wait=False)
+    print("lyrydber: shipwright aks")
 
     with self.command_group('shipwright vm', compute_vm_sdk) as g:
         g.custom_command('create', 'create_vm', transform=transform_vm_create_output, supports_no_wait=True, table_transformer=deployment_validate_table_format, validator=azl_process_vm_create_namespace, exception_handler=handle_template_based_exception)
+    print("lyrydber: shipwright vm")
--- src/shipwright/azext_shipwright/custom.py
+++ src/shipwright/azext_shipwright/custom.py
@@ -4,9 +4,113 @@
 # --------------------------------------------------------------------------------------------
 
 from azure.cli.command_modules.vm.custom import create_vm
+from azext_aks_preview.custom import aks_create
+
+import sys
+from azure.cli.core.extension import get_extension_path
+sys.path.append(get_extension_path('aks-preview'))
 
 def echo_shipwright(cmd, echo_back):
     print(f"echo: '{echo_back}'")
 
 def create_vm_shipwright(cmd, vm_name):
     print(f"Creating VM '{vm_name}'...")
+
+def get_resource_groups_client(cli_ctx, subscription_id=None):
+    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from azure.cli.core.profiles import ResourceType
+
+    return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES,
+                                   subscription_id=subscription_id).resource_groups
+
+def get_rg(cli_ctx, resource_group_name, subscription_id=None):
+    from knack.util import CLIError
+    groups = get_resource_groups_client(cli_ctx, subscription_id=subscription_id)
+    # Just do the get, we don't need the result, it will error out if the group doesn't exist.
+    rg = groups.get(resource_group_name)
+    if rg is None:
+        raise CLIError(f"Resource group {resource_group_name} not found.")
+    return rg
+
+    # from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    # from azure.cli.core.profiles import ResourceType
+
+    # resource_group = get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES).resource_groups.get(resource_group_name)
+    # return resource_group.location
+
+
+def shipwright_aks_create(cmd, resource_group, cluster_name):
+    azsecpack_ua_rg = "azsecpackautoconfigrg"
+    azsecpack_uai_basename="azsecpackautoconfigua-"
+
+    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from azure.cli.core.commands.parameters import get_resources_in_subscription
+    from azure.cli.core.profiles import ResourceType
+    from azure.cli.core.profiles import CustomResourceType
+
+    from azext_aks_preview._client_factory import cf_managed_clusters
+
+    print(f"Creating AKS cluster '{cluster_name}' in resource group '{resource_group}'...")
+    print("AKS cluster created.")
+
+    # get cluster resource object
+    # get cluster location, nodepool resource group, and nodepool name(s)
+    # build azsecpack UAI name
+    # assign azsecpack UAI to cluster's nodepool(s)
+
+    rg = get_rg(cmd.cli_ctx, resource_group)
+    location = "Unknown"
+    if rg is not None:
+        location = rg.location
+
+    print(f"Resource group location: {location}")
+
+    print(f"Resource group: {rg}")
+
+    # rg_location = get_mgmt_service_client(cmd.cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES,
+    #                         subscription_id=subscription_id).resource_groups.
+
+    # resources = get_mgmt_service_client(cmd.cli_ctx, ResourceType.MGMT_COMPUTE,
+    #                                subscription_id=None).resources
+
+    # print(f"Resources: {resources}")
+    # from azure.cli.command_modules.acs.custom import aks_agentpool_list
+    # from azure.cli.command_modules.acs._format import aks_agentpool_list_table_format
+    client = cf_managed_clusters(cmd.cli_ctx)#cf_agent_pools(cmd.cli_ctx)
+    # print(f"Client: {client}")
+
+    # results = aks_agentpool_list(cmd, client, resource_group, cluster_name)
+    # nodepool_list = aks_agentpool_list_table_format(results)
+    # #nodepool_list = list(client.list(resource_group, cluster_name))
+    # print(f"List: {nodepool_list}")
+    # # print(f"Instance: {instance}")
+
+    from  azure.mgmt.containerservice import ContainerServiceClient
+
+    from azure.identity import DefaultAzureCredential
+    from azure.cli.core import get_default_cli
+
+    from azure.cli.core.commands.client_factory import get_subscription_id, get_az_user_agent
+    subscription = get_subscription_id(cmd.cli_ctx)
+    print(f"Subscription: {subscription}")
+
+    from azure.cli.command_modules.acs.custom import aks_show
+    result = aks_show(cmd, client, resource_group, cluster_name)
+
+    print(f"Result: {result}\n\n{type(result)}\n\n")
+
+    print(f"AgentPool: {result.agent_pool_profiles}")
+
+
+
+    # import sys
+
+    # print(f"sys.path: {sys.path}")
+    # sys.path.append(get_extension_path('aks-preview'))
+
+    # print(f"sys.path: {sys.path}")
+
+
+    # print(f"Get Extension path {get_extension_path('aks-preview')}")
+    # client = ContainerServiceClient(identity=DefaultAzureCredential().get_token("openid"), subscription_id=subscription)
+    # print(f"Client: {client}")
