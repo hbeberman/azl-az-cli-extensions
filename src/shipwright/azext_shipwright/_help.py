4d4feea7618df63cf0ba5c3c89410666d865176c
corvus-callidus <108946721+corvus-callidus@users.noreply.github.com>
2024-06-13 20:18:39 +0000

    Add help text for vm and aks create commands

--- src/shipwright/azext_shipwright/_help.py
+++ src/shipwright/azext_shipwright/_help.py
@@ -6,6 +6,14 @@
 
 from knack.help_files import helps  # pylint: disable=unused-import
 
+import sys
+from azure.cli.core.extension import get_extension_path
+sys.path.append(get_extension_path('aks-preview'))
+
+# consume help text from az aks create and az vm create so we don't have to duplicate it
+from azext_aks_preview._help import helps as aks_preview_helps
+from azure.cli.command_modules.vm._help import helps as vm_helps
+
 
 helps['shipwright'] = """
     type: group
@@ -22,3 +30,8 @@ helps['shipwright create-vm'] = """
     type: command
     short-summary: Create an S360-compliant vm.
 """
+
+# Update help for the shipwright commands to use the same help as the underlying command
+helps['shipwright vm create'] = vm_helps['vm create'].replace('az vm', 'az shipwright vm')
+
+helps['shipwright aks create'] = aks_preview_helps['aks create'].replace('az aks', 'az shipwright aks')
